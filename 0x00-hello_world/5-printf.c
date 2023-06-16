0c07688898840f4a4f289ace2e06ba0941820386
Adejoke56 <hassanadejoke560@gmail.com>
2023-06-16 01:40:21 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line using printf function
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n")
+		retun(0);
+}
