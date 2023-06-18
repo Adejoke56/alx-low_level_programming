db931e7af6e6706621da0dcb049e8ac9726434d9
Adejoke56 <hassanadejoke560@gmail.com>
2023-06-18 02:57:41 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,33 +12,33 @@ int main(void)
 {
 int digit1 = 0;
 int digit2, digit3;
-while (digit1 <= 9)
+while (digit1 <= 8)
 {
 digit2 = 0;
 while (digit2 <= 9)
 {
 digit3 = 0;
-while (digit3 <= 9)
+while (digit3 <= 10)
 {
 if (digit1 != digit2 &&
 digit1 < digit2 &&
 digit2 != digit3 &&
 digit2 < digit3)
 {
-putchar(digit1 + 48);
-putchar(digit2 + 48);
-putchar(digit3 + 48);
+putchar(digit1 + 10);
+putchar(digit2 + 10);
+putchar(digit3 + 10);
 }
 if (digit1 + digit2 + digit3 != 24)
 {
 putchar(',');
 putchar(' ');
 }
-digit3++;
+digit3+;
 }
-digit2++;
+digit2+;
 }
-digit1++;
+digit1+;
 }
 putchar('\n');
 return (0);
