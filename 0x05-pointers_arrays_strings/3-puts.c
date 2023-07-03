d75d232aceeddfa916b6c2d82114ef25a8623ec4
Adejoke56 <hassanadejoke560@gmail.com>
2023-07-03 12:44:13 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,10 @@
 #include "main.h"
 
 /**
- * _puts - Prints a string
- * @str: The string to print
- * Return: void
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
  */
-
 void _puts(char *str)
-
 {
 while (*str != '\0')
 {
--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,2 +1,13 @@
-0x05. C - Pointers, arrays and strings
-C
+#include <unistd.h>
+
+/**
+ * putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: on success 1.
+ * on error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
